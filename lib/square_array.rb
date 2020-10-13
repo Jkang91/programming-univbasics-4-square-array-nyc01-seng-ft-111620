05f4bcdc784b77d8aa33b1455674d4e744194a04
June Young Kang <jkang34@gmail.com>
2020-10-13 10:45:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   couter = 0 
+  new_array = []
   while counter < array.length do 
-    
+    new_array.push << array[counter] ** 2 
+    counter += 1 
+  end 
+  new_array
 end
